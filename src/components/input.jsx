65853eee9463ddd0e78506175ec04ed2e1ec86e6
hello-phj <hejae0419@gmail.com>
2024-12-18 23:57:51 +0900

    hello

---
+++ src/components/input.jsx
@@ -0,0 +1,10 @@
+function Test({ hello }) {
+  return (
+    <>
+      <div>helloworld!</div>
+      <div>{hello}</div>
+    </>
+  );
+}
+
+export default Test;
---
+++ src/components/test.jsx
@@ -0,0 +1,12 @@
+// components/Test2.js
+import Test from "./input";
+
+function Test2() {
+  return (
+    <div>
+      <Test hello="hello" />
+    </div>
+  );
+}
+
+export default Test2;
